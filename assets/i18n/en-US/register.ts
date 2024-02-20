ce29252172009fca6523dbecb8b0b3c4cd8f6d72
Jarurat <it_561413013@hotmail.com>
2024-02-20 18:22:45 +0700

    x

---
+++ assets/i18n/en-US/register.ts
@@ -0,0 +1,15 @@
+export default {
+  registerPage: {
+    coperation: 'M-Inter Freight',
+    title: 'Register',
+    submit: 'Register',
+    email: 'Email',
+    password: 'Password',
+    confirmPassword: 'Confirm Password',
+    other: 'Already have an account?',
+    resultMessage: {
+      title: 'Registration success',
+      content: 'Your account is successfully registered, and will return to the login page to log in.',
+    },
+  },
+};
